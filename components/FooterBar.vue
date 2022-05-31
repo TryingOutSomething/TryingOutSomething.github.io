0af6a540e4eb03f7c5bde5b45443a42f56648bbf
TryingOutSomething <jan.owyeong123@gmail.com>
2022-05-31 17:00:13 +0800

    fix: adjust responsive design for footer

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -1,33 +1,88 @@
 <template>
   <footer class="bg-p-black">
-    <div class="text-p-white flex flex-col justify-between py-12 px-5 sm:hidden">
-      <div>
-        <p class="capitalize">mobile created with</p>
+    <!-- Mobile -->
+    <div
+      class="text-p-white md:hidden
+      flex flex-col justify-center items-center
+      py-12 sm:py-5 px-5 sm:px-0"
+    >
+      <client-only>
+        <div class="sm:flex sm:flex-row sm:items-center sm:justify-center sm:space-x-3">
+          <p class="text-lg">Connect with me:</p>
+
+          <div class="flex flex-row space-x-3 justify-center my-5">
+            <a :href="emailLink" aria-label="Email" rel="noopener noreferrer" target="_blank">
+              <email-icon/>
+            </a>
+            <a :href="githubLink" aria-label="Github" rel="noopener noreferrer" target="_blank">
+              <github-icon/>
+            </a>
+            <a :href="linkedInLink" aria-label="LinkedIn" rel="noopener noreferrer" target="_blank">
+              <linkedin-icon/>
+            </a>
+          </div>
+        </div>
+      </client-only>
+
+      <div class="sm:hidden">
+        <div class="text-center text-sm mb-1">© 2022 Jan Owyeong Guo Yong</div>
+        <div class="text-center text-sm">Singapore, Singapore</div>
       </div>
-      <div>© 2022 Jan Owyeong Guo Yong | Singapore, Singapore</div>
-      <div>
-        <p>Connect with me:</p>
-      </div>
+
+      <div class="text-center text-sm hidden sm:block">© 2022 Jan Owyeong Guo Yong | Singapore, Singapore</div>
+
+      <client-only>
+        <div class="mt-5 sm:mt-3 text-xs flex flex-row items-center justify-center space-x-2">
+          <p class="capitalize">created with:</p>
+          <nuxt-icon :size="mobileToolsIconSize"/>
+          <tailwind-icon :size="mobileToolsIconSize"/>
+        </div>
+      </client-only>
     </div>
 
-    <div class="text-p-white hidden justify-between py-3 px-5 sm:flex">
-      <div class="flex flex-row space-x-2">
-        <div class="capitalize">created with:</div>
-        <div>img</div>
-        <div>img</div>
-      </div>
+    <!-- Desktop -->
+    <div
+      class="text-p-white text-xs hidden
+      md:flex justify-between py-2.5 px-7 lg:px-20 items-center"
+    >
+      <client-only>
+        <div class="flex flex-row space-x-2.5 items-center justify-center">
+          <div class="capitalize font-light">created with:</div>
+          <nuxt-icon/>
+          <tailwind-icon/>
+        </div>
+      </client-only>
+
       <div>© 2022 Jan Owyeong Guo Yong | Singapore, Singapore</div>
-      <div class="flex flex-row space-x-2">
-        <div>Connect with me:</div>
-        <div>img</div>
-        <div>img</div>
-        <div>img</div>
-      </div>
+
+      <client-only>
+        <div class="flex flex-row space-x-2.5 items-center justify-center">
+          <div>Connect with me:</div>
+          <a :href="emailLink" aria-label="Email" rel="noopener noreferrer" target="_blank">
+            <email-icon/>
+          </a>
+          <a :href="githubLink" aria-label="Github" rel="noopener noreferrer" target="_blank">
+            <github-icon/>
+          </a>
+          <a :href="linkedInLink" aria-label="LinkedIn" rel="noopener noreferrer" target="_blank">
+            <linkedin-icon/>
+          </a>
+        </div>
+      </client-only>
     </div>
   </footer>
 </template>
 <script>
 export default {
-  name: 'FooterBar'
+  name: 'FooterBar',
+  data() {
+    return {
+      mobileToolsIconSize: 18,
+      mobileConnectIconSize: 32,
+      emailLink: 'mailto:janowyeong@hotmail.com',
+      githubLink: 'https://github.com/TryingOutSomething',
+      linkedInLink: 'https://www.linkedin.com/in/jan-owyeong-guo-yong-04333774/',
+    };
+  }
 };
 </script>
