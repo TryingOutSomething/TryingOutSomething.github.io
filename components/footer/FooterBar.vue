c3c595ddfad3a464b431ebd151d46c154c178255
TryingOutSomething <jan.owyeong123@gmail.com>
2022-06-02 17:13:07 +0800

    feat: decouple mobile & desktop footer to two file

---
+++ components/footer/FooterBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="bg-p-black">
+    <mobile-footer
+      :sns-links="snsLinks"
+      :tools-icon-size="mobileToolsIconSize"
+      class="md:hidden
+      flex flex-col justify-center items-center
+      py-12 sm:py-5 px-5 sm:px-0"
+    />
+
+    <desktop-footer :sns-links="snsLinks" class="hidden md:flex justify-between py-2.5 px-7 lg:px-20 items-center"/>
+  </footer>
+</template>
+<script>
+import MobileFooter from '~/components/footer/MobileFooter';
+import DesktopFooter from '~/components/footer/DesktopFooter';
+
+export default {
+  name: 'FooterBar',
+  components: { DesktopFooter, MobileFooter },
+  data() {
+    return {
+      snsLinks: {
+        email: {
+          url: 'mailto:janowyeong@hotmail.com',
+          ariaLabel: 'Email'
+        },
+        github: {
+          url: 'https://github.com/TryingOutSomething',
+          ariaLabel: 'Github'
+        },
+        linkedIn: {
+          url: 'https://www.linkedin.com/in/jan-owyeong-guo-yong/',
+          ariaLabel: 'LinkedIn'
+        }
+      },
+      mobileToolsIconSize: 18
+    };
+  }
+};
+</script>
