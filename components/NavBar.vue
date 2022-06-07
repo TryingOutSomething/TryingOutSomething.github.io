41222df17ad3ddbfa53b4fa92af5dcad96ea58b6
janowyeong <janowyeong@sph.com.sg>
2022-06-07 13:10:49 +0800

    feat: change mobile menu icon on click

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -14,14 +14,16 @@
             @click="toggleMenuItemsVisibility"
           >
             <svg
-              aria-hidden="true" class="block h-6 w-6" fill="none" stroke="currentColor"
-              stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M4 6h16M4 12h16M4 18h16" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-            <svg
-              aria-hidden="true" class="hidden h-6 w-6" fill="none" stroke="currentColor"
+              v-if="showMenuMobile"
+              aria-hidden="true" class="h-6 w-6" fill="none" stroke="currentColor"
               stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M6 18L18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <svg
+              v-else
+              aria-hidden="true" class="h-6 w-6" fill="none" stroke="currentColor"
+              stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4 6h16M4 12h16M4 18h16" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
           </button>
           <!-- end mobile menu -->
